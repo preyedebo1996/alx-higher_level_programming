95c4c0b7d49dd27f38da2e4fa3bb97cf7ab2deb6
preyedebo1996 <preyedebo@gmail.com>
2022-07-07 01:15:55 -0700

    Task 4

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+if __name__ == '__main__':
+    import sys
+    argv = int(sys.argv[1:])
+    index = 0
+    arg_len = len(argv)
+    if len(argv) is 0:
+        print("0")
+    while index <= arg_len:
+        total_sum += argv[index]
+        index ++
+    print("{}".format(total_sum))
