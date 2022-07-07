1f4dad1a540b915cd86d544b20a1d46f3bf4cc4a
preyedebo1996 <preyedebo@gmail.com>
2022-07-07 00:39:35 -0700

    Task 2-3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/ussr/bin/python3
+if __name__ == '__main__':
+    import sys
+    argv = sys.argv[1:]
+    len_arg = len(argv)
+    count = 1
+    if len_arg == 0:
+        print("{:d} arguments.".format(len_arg))
+    elif len_arg == 1:
+        print("{:d} argument:".format(len_arg))
+        print("{:d}: {:s}".format(len_arg, argv[1])
+    else:
+        print("{} arguments:".format(len_arg))
+        while count <= len_arg:
+            print("{}: ".format(count, argv[count]))
+            count++
